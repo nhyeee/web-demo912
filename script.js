f8910f727221aaca7b7771a7305024cd2ffd506a
Trần Ngọc Huế <153601738+nhyeee@users.noreply.github.com>
2025-08-27 11:42:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").addEventListener("click", () => {
+    alert("hehehe");
+  });
+  
+  document.getElementById("title").addEventListener("click", () => {
+    document.body.style.background = "lightyellow";
+  });
